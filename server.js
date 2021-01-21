06846ba3aba1137b518ab2501ea0d24f0aab430a
linxiaozhou <kkdestiny@icloud.com>
2021-01-21 22:26:11 +0800

    feature: support dotenv

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const gateway = require('express-gateway');
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
 
 gateway()
   .load(path.join(__dirname, 'config'))
